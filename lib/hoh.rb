e22dac7e9d3c4559ce5a0ef28b9976e8bf2a2e3a
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 18:54:57 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,15 +32,7 @@
 
 def naming_system
   PORTION_1 = {
-    {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
+    
   }
   #  Kingdom
   #  Phylum
